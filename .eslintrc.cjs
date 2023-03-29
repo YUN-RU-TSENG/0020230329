491a08baaf0657ef87e2d476770fad562e765c61
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 14:44:33 +0800

    chore: initial and setting Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:react/jsx-runtime"
+        "plugin:react/jsx-runtime",
+        "prettier",
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+    plugins: ["react"],
+    rules: {},
+    settings: {
+        react: {
+            version: "18.2.0",
+        },
     },
-    "settings": {
-        "react": {
-            "version": "18.2.0"
-        }
-    }
 }
---
+++ .prettierrc.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+    semi: false, // Print semicolons at the ends of statements. false -> don't print
+}
