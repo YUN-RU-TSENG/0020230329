1260ab7fcc04b1471e5b116899884027e313b0cc
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 14:59:45 +0800

    chore: setting alias on vite and jsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((config) => ({
     plugins: [react()],
-})
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    // build production 時刪除 console、debugger
+    esbuild: {
+        drop: config.mode === "production" ? ["console", "debugger"] : [],
+    },
+}))
