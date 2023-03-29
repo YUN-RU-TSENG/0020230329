f6c2075723ac8b9b96bf7739331c8b990da1b7d1
YUN-RU-TSENG <coco002197@gmail.com>
2023-03-29 17:36:10 +0800

    feat: base router

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+    Route,
+} from "react-router-dom"
+
+import Home from "./pages/Home"
+import EditTask from "./pages/EditTask"
+import CreateTask from "./pages/CreateTask"
+import NotFound from "./pages/NotFound"
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="edit-task" element={<EditTask />} />
+            <Route path="create-task" element={<CreateTask />} />
+            <Route path="*" element={<NotFound />} />
+        </Route>
+    )
+)
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+        <div>
+            <RouterProvider router={router} />
         </div>
     )
 }
